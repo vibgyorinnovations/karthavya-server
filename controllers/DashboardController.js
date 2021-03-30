7a8bfc08384b93b1013ad165ba9221bc1a5a8965
Devamuthan <devamuthansankar@gmail.com>
2021-03-31 02:43:33 +0530

    Cors configured

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -1,60 +1,63 @@
-const express = require( 'express' );
-const router = express.Router();
-const bodyParser = require( 'body-parser' );
-const DashboardDTO = require( '../dto/DashboardDTO' );
-const DashboardService = require( '../services/DashboardService' );
-const log4js = require( "log4js" );
-const logger = log4js.getLogger();
-const date = new Date();
+const express = require('express')
+const router = express.Router()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const DashboardDTO = require('../dto/DashboardDTO')
+const DashboardService = require('../services/DashboardService')
+const log4js = require('log4js')
+const logger = log4js.getLogger()
+const date = new Date()
 
-logger.level = 'info';
+logger.level = 'info'
 
-let dashboardDTO = new DashboardDTO();
-const dashboardService = new DashboardService();
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({ extended: false }))
 
-router.use( bodyParser.json() );
-router.use( bodyParser.urlencoded( { extended: false } ) );
+// Configuring CORS
+let corsConfig = { /*origin: 'http://localhost:3000/dashboard'*/ }
+
+let dashboardDTO = new DashboardDTO()
+const dashboardService = new DashboardService()
 
 // Request to Fetch Present Day's Data
-router.get( '/', ( req, res ) => {
-    req.params.date = String( date.getDate() ) + '-' + String( date.getMonth() + 1 ) + '-' + String( date.getFullYear() );
-    getDashboardData( req, res ).then( () => res.end() );
-} );
+router.get('/', cors(corsConfig), (req, res) => {
+    req.params.date = String(date.getDate()) + '-' + String(date.getMonth() + 1) + '-' + String(date.getFullYear())
+    getDashboardData(req, res).then(() => res.end())
+})
 
 // Request to Add Data into the Database
-router.post( '/', ( req, res ) => addDashboardData( req, res ) );
+router.post('/', cors(corsConfig), (req, res) => addDashboardData(req, res))
 
 // Request to Fetch Current Week's Data
-router.get( '/week-data', ( req, res ) => getWeeklyDashboardData( req, res ) );
+router.get('/week-data', cors(corsConfig), (req, res) => getWeeklyDashboardData(req, res))
 
 // Request to Fetch a Particular Day's Data
-router.get( '/:date', ( req, res ) => getDashboardData( req, res ) );
+router.get('/:date', cors(corsConfig), (req, res) => getDashboardData(req, res))
 
-let addDashboardData = async( req, res ) => {
-    logger.info( 'Entering | DashboardController::addDashboardData' );
-    dashboardDTO = await dashboardService.addDashboardData( req.body, dashboardDTO );
-    res.json( dashboardDTO.json );
-    logger.info( 'Exiting | DashboardController::addDashboardData' );
-    res.end();
+let addDashboardData = async (req, res) => {
+    logger.info('Entering | DashboardController::addDashboardData')
+    dashboardDTO = await dashboardService.addDashboardData(req.body, dashboardDTO)
+    res.json(dashboardDTO.json)
+    logger.info('Exiting | DashboardController::addDashboardData')
+    res.end()
 }
 
-let getDashboardData = async( req, res ) => {
-    req.session.accountId = 'sjdhiadsivnvk';
-    logger.info( 'Entering | DashboardController::getDashboardData' );
-    dashboardDTO = await dashboardService.getDashboardData( req.params.date, req.session.accountId, dashboardDTO );
-    res.json( dashboardDTO.json );
-    logger.info( 'Exiting | DashboardController::getDashboardData' );
-    res.end();
+let getDashboardData = async (req, res) => {
+    req.session.accountId = 'sjdhiadsivnvk'
+    logger.info('Entering | DashboardController::getDashboardData')
+    dashboardDTO = await dashboardService.getDashboardData(req.params.date, req.session.accountId, dashboardDTO)
+    res.json(dashboardDTO.json)
+    logger.info('Exiting | DashboardController::getDashboardData')
+    res.end()
 }
 
-let getWeeklyDashboardData = async( req, res ) => {
-    req.session.accountId = 'sjdhiadsivnvk';
-    logger.info( 'Entering | DashboardController::getWeeklyDashboardData' );
-    dashboardDTO = await dashboardService.getWeeklyDashboardData( req.session.accountId, dashboardDTO );
-    res.json( dashboardDTO.json );
-    logger.info( 'Exiting | DashboardController::getWeeklyDashboardData' );
-    res.end();
+let getWeeklyDashboardData = async (req, res) => {
+    req.session.accountId = 'sjdhiadsivnvk'
+    logger.info('Entering | DashboardController::getWeeklyDashboardData')
+    dashboardDTO = await dashboardService.getWeeklyDashboardData(req.session.accountId, dashboardDTO)
+    res.json(dashboardDTO.json)
+    logger.info('Exiting | DashboardController::getWeeklyDashboardData')
+    res.end()
 }
 
-
-module.exports = router;
+module.exports = router
--- index.js
+++ index.js
@@ -1,50 +1,49 @@
-const express = require( 'express' );
-const app = express();
-const dashboardController = require( './controllers/DashboardController' );
-const bodyParser = require( 'body-parser' );
-const log4js = require( "log4js" );
-const logger = log4js.getLogger();
-const sessions = require( 'express-session' );
-const cors = require('cors');
+const express = require('express')
+const app = express()
+const dashboardController = require('./controllers/DashboardController')
+const bodyParser = require('body-parser')
+const log4js = require('log4js')
+const logger = log4js.getLogger()
+const sessions = require('express-session')
 
-logger.level = 'info';
+logger.level = 'info'
 
-const port = 5000;
+const port = 5000
 
-logger.info('Server Available at port:'+port);
+logger.info('Server Available at port:' + port)
 
 //app.use(bodyParser.json());
-app.use( bodyParser.urlencoded( { extended: false } ) )
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use( sessions( {
+app.use(sessions({
     secret: 'secret-key',
     resave: true,
     saveUninitialized: false,
-} ) );
+}))
 
 // Get Request from /
-app.get( '/', function ( req, res ) {
-    logger.info( 'Get Request to /' );
-    res.end( 'Server Running Successfully at port ' + port );
-} );
+app.get('/', function (req, res) {
+    logger.info('Get Request to /')
+    res.end('Server Running Successfully at port ' + port)
+})
 
 // Login Routes
-app.get( '/login', function ( req, res ) {
-    logger.info( 'Get Request to /login' );
-    res.end( 'Login Page' );
-} );
+app.get('/login', function (req, res) {
+    logger.info('Get Request to /login')
+    res.end('Login Page')
+})
 
 // Dashboard Routes
-app.use( '/dashboard', dashboardController );
+app.use('/dashboard', dashboardController)
 
 // Handling 404 Requests
-app.use( function ( req, res ) {
-    logger.info( '404 error... Page not found' );
-    res.json( {
+app.use(function (req, res) {
+    logger.info('404 error... Page not found')
+    res.json({
         status: '404',
         description: 'Page Not Found'
-    } );
-    res.end();
-} );
+    })
+    res.end()
+})
 
-app.listen( port );
+app.listen(port)
